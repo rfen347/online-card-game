ef6bea380bcd85a56b7b39dc957fb43486970e54
RobinFeng <rfen347@aucklanduni.ac.nz>
2016-04-11 12:13:10 +1200

    Changed index.html

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var db = require('mysql');
 // app.get('/', function (req, res) {
 //   // res.send('GET request to the homepage');
 // });
+
+/*## Main things to learn:
+Full Stack development (freelancing mindset)<br />
+Portfolio creation<br /> JavaScript language<br /> App Development(web,desktop,mobile).<br />*/
 app.use(express.static(__dirname));
 
 app.listen(3000);
